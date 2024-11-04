f701a0f6750bb437508b21914a92abb843e3e6fd
kaua-sousaa <kauasousa15_rj@hotmail.com>
2024-11-03 22:51:13 -0300

    inteligente aprendendo com jogadas

--- Inteligente.py
+++ Inteligente.py
@@ -1,19 +1,22 @@
 import random
 #14 lugares 
 #0 numero da jogada. Cada partida reseta esse numero
-#1 a 9 = posicoes para jogada
-#10 Vencedor
-#11 nº partida
-#12, 13, 14 = X, V, O
+#1 -> [0,8] = posicoes para jogada
+#2 Vencedor
+#3 nº partida
+#4, 5, 6 = X, V, O
+#7 rank
+#8 posicao jogada
 # #vetor[0] % 2 == 0 >> X
 
 #-1 = X /// 1 = O
 vetorAtual = [0,
+        [-1,-1,-1,
         -1,-1,-1,
-        -1,-1,-1,
-        -1,-1,-1,
+        -1,-1,-1],
         0,0,
-        0,0,0,]
+        0,0,0,
+        0,0]
 
 ranks = [[0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0],
          [0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0],
@@ -21,7 +24,9 @@ ranks = [[0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0],
 
 
 #guardar cada jogada aqui
-todosJogos = []
+
+teste = []
+vetorJogadas = []
 
 def jogo_da_velha(vetorAtual):
     quantidade_jogos = int(input("Quantos jogos deseja ? "))
@@ -29,38 +34,42 @@ def jogo_da_velha(vetorAtual):
     dados_para_escrever = []
     
     for _ in range(quantidade_jogos):
+        jogadas_utilizadas = []
+
         jogar = True
         vetorAtual[0] = 0
-        vetorAtual[1:10] = [-1]*9
-        vetorJogadas = []
+        vetorAtual[1] = [-1]*9
+        vetorJogadas.append([vetorAtual[1].copy(), vetorAtual[3],vetorAtual[7], vetorAtual[8], len(vetorJogadas)+1])
+        vetorAtual[0]+=1
         while (jogar == True):
             if (opcao == 1):
-                if (vetorAtual[0] % 2 == 0):   
+                """ if (vetorAtual[0] % 2 == 0):   
                     posicao = jogador_inteligente(vetorAtual, ranks)    
                     vetorAtual[posicao] = 1
                     jogador = 1
                 else:
                     posicao = jogador_inteligente(vetorAtual, ranks)
                     vetorAtual[posicao] = 2
-                    jogador = 2
+                    jogador = 2 """
             if (opcao == 2):         
-                if (vetorAtual[0] % 2 == 0):      
-                    posicao = jogador_inteligente(vetorAtual, ranks)
-                    vetorAtual[posicao] = 1
+                if (vetorAtual[0] % 2 != 0):      
+                    posicao = jogador_inteligente(vetorAtual, jogadas_utilizadas)
+                    vetorAtual[1][posicao] = 1
                     jogador = 1
                 else:
                     posicao = jogada_aleatoria(vetorAtual)
-                    vetorAtual[posicao] = 2
+                    vetorAtual[1][posicao] = 2
                     jogador = 2
             if (opcao == 3):
                 if (vetorAtual[0] % 2 == 0):
+                    posicao = jogador_inteligente(vetorAtual, jogadas_utilizadas)
+                    vetorAtual[1][posicao] = 1
+                    jogador = 1  
+                else:                   
                     posicao = jogada_aleatoria(vetorAtual)
-                    vetorAtual[posicao] = 2
+                    vetorAtual[1][posicao] = 2
                     jogador = 2
-                else:
-                    posicao = jogador_inteligente(vetorAtual, ranks)
-                    vetorAtual[posicao] = 1
-                    jogador = 1                  
+                    """                 
             if (opcao == 4):
                 if (vetorAtual[0] % 2 == 0):
                     posicao = jogador_inteligente(vetorAtual, ranks)
@@ -78,16 +87,16 @@ def jogo_da_velha(vetorAtual):
                 else:
                     posicao= jogador_inteligente(vetorAtual, ranks)
                     vetorAtual[posicao] = 1
-                    jogador = 1
+                    jogador = 1 """
             
-            jogar = vencedor(vetorAtual, jogador)    
-            vetorJogadas.append([vetorAtual.copy(), posicao]) #JOGADA E POSICAO
+            jogar = vencedor(vetorAtual, jogador)
+            vetorJogadas.append([vetorAtual[1].copy(), vetorAtual[3],vetorAtual[7], posicao, len(vetorJogadas)+1]) #tabuleiro, nº partida, rank e posicao jogada
             vetorAtual[0]+=1
 
-        atualizar_rank(vetorAtual, vetorJogadas, opcao)                             
-        vetorAtual[11]+=1 #+1 partida
+        atualizar_rank(vetorAtual, vetorJogadas, jogadas_utilizadas, opcao)                             
+        vetorAtual[3]+=1 #+1 partida
         dados_para_escrever.append(";".join(map(str, vetorAtual)))
-    
+        teste.append(jogadas_utilizadas)
     if opcao == 1:
         nomeCsv = 'inteligente_inteligente.csv'
     elif opcao ==2:
@@ -103,108 +112,94 @@ def jogo_da_velha(vetorAtual):
         for linha in dados_para_escrever:
             f.write(linha + "\n")
 
+    
 
-def atualizar_rank(vetorAtual, vetorJogadas, opcao):
-    # print(vetorAtual)
-    for i, jogadas in enumerate(vetorJogadas):
-            posicao_jogada = jogadas[1]-1
-            if opcao ==2 or opcao == 4:
-                if (i%2 ==0):
-                    if vetorAtual[10] == 1:
-                        ranks[i][posicao_jogada] += 1
-                    elif vetorAtual[10] == 2:
-                        ranks[i][posicao_jogada] -= 1
-                    else:
-                        ranks[i][posicao_jogada] +=1
-            elif opcao ==3 or opcao == 5:
-                if (i%2 !=0):
-                    if vetorAtual[10] == 1:
-                        ranks[i][posicao_jogada] += 1
-                    elif vetorAtual[10] == 2:
-                        ranks[i][posicao_jogada] -= 1
-                    else:
-                        ranks[i][posicao_jogada] +=1
+def atualizar_rank(vetorAtual, vetorJogadas, jogadas_utilizadas, opcao):
+    # print(jogadas_utilizadas)
+    for jogadas in vetorJogadas:
+        if jogadas[4] in jogadas_utilizadas:
+            if vetorAtual[2] == 1:
+                jogadas[2]+=1
+            elif vetorAtual[2] == 2:
+                jogadas[2]-=1
+            else:
+                jogadas[2]+=0
 
-            elif opcao ==1:
-                    if (i%2 ==0):
-                        if vetorAtual[10] == 1:
-                            ranks[i][posicao_jogada] += 1
-                        elif vetorAtual[10] == 2:
-                            ranks[i][posicao_jogada] -= 2
-                        else:
-                            ranks[i][posicao_jogada] +=1
-                    else:
-                        if vetorAtual[10] == 2:
-                            ranks[i][posicao_jogada] += 1
-                        elif vetorAtual[10] == 1:
-                            ranks[i][posicao_jogada] -= 2
-                        else:
-                            ranks[i][posicao_jogada] +=1
+        if vetorAtual[3] == jogadas[1]: ##Se for a mesma partida
+            if vetorAtual[2] == 1: #Se foi vencedor
+                jogadas[2]+=1 #atualizando rank
+            elif vetorAtual[2] == 2:
+                jogadas[2]-=1
+            else:
+                jogadas[2]+=0
 
-            ranks[i][posicao_jogada] = max(-10, min(10, ranks[i][posicao_jogada]))
             
-            
-def jogador_inteligente(vetorAtual, ranks):
-    if vetorAtual[11] == 0:
-        melhor_jogada = jogada_aleatoria(vetorAtual)
-        return melhor_jogada
-
-    jogada = vetorAtual[0]
-    rankJogada = ranks[jogada]
-    maiorRank = -999
-    encontrou = False
-
-    for i, rank in enumerate(rankJogada):
-        if rank > maiorRank and vetorAtual[i+1] == -1:
-            maiorRank = rank
-            melhor_jogada = i+1 #Pq aqui, ele varre o rank que tem 9 posições, o tabuleiro tem 9 mas começa na 1
-            encontrou = True
-
-    if encontrou == False:
+def jogador_inteligente(vetorAtual, jogadas_utilizadas):
+    if vetorAtual[3] == 0:
         melhor_jogada = jogada_aleatoria(vetorAtual)
         return melhor_jogada
     
+    jogada_utilizada = None
+    rank = 0
+    achou = False
+    for i, jogos in enumerate(vetorJogadas):
+        tabuleiro = jogos[0]
+
+        if vetorAtual[1] == tabuleiro and jogos[2] > rank and vetorAtual[1][vetorJogadas[i+1][3] == -1]:
+            #Se tabuleiro é igual E Rank for o Maior E Posicao estiver disponivel no vetorAtual
+            rank = jogos[2]
+            melhor_jogada = vetorJogadas[i + 1][3]
+            jogada_utilizada = vetorJogadas[i][4] #id do vetor utilizado
+            achou = True
+  
+    if achou == False:
+        melhor_jogada = jogada_aleatoria(vetorAtual)   
+
+    jogadas_utilizadas.append(jogada_utilizada)
     return melhor_jogada
     
 
 def vencedor(vetor, jogador, simulacao=False):
     #print("print vetor[0]: ", vetor[0])
-    if (   (vetor[1] ==  vetor[2] == vetor[3] ==jogador) #linha1
-        or (vetor[4] ==  vetor[5] == vetor[6] ==jogador) #linha2
-        or (vetor[7] ==  vetor[8] == vetor[9] ==jogador) #linha3
-        or (vetor[1] ==  vetor[4] == vetor[7] ==jogador) #coluna1
-        or (vetor[2] ==  vetor[5] == vetor[8] ==jogador) #coluna2
-        or (vetor[3] ==  vetor[6] == vetor[9] ==jogador) #coluna3
-        or (vetor[1] ==  vetor[5] == vetor[9] ==jogador) #diagonal 1
-        or (vetor[3] ==  vetor[5] == vetor[7] ==jogador)): #diagonal 2
+    tabuleiro = vetorAtual[1]
+    if (   (tabuleiro[0] ==  tabuleiro[1] == tabuleiro[2] ==jogador) #linha1
+        or (tabuleiro[3] ==  tabuleiro[4] == tabuleiro[5] ==jogador) #linha2
+        or (tabuleiro[6] ==  tabuleiro[7] == tabuleiro[8] ==jogador) #linha3
+        or (tabuleiro[0] ==  tabuleiro[3] == tabuleiro[6] ==jogador) #coluna1
+        or (tabuleiro[1] ==  tabuleiro[4] == tabuleiro[7] ==jogador) #coluna2
+        or (tabuleiro[2] ==  tabuleiro[5] == tabuleiro[8] ==jogador) #coluna3
+        or (tabuleiro[0] ==  tabuleiro[4] == tabuleiro[8] ==jogador) #diagonal 1
+        or (tabuleiro[2] ==  tabuleiro[4] == tabuleiro[6] ==jogador)): #diagonal 2
         if not simulacao:
             #print(f"O jogador {jogador} ganhou")
             #vetor[12] = jogador
             if jogador == 1:
-                vetor[10] = 1                   
-                vetor[12]+=1 #Mais uma vitoria para jogador X
+                vetor[2] = 1                   
+                vetor[4]+=1 #Mais uma vitoria para jogador X
             elif jogador == 2:
-                vetor[10] = 2
-                vetor[14]+=1 #Mais uma vitoria para jogador O
+                vetor[2] = 2
+                vetor[5]+=1 #Mais uma vitoria para jogador O
             return False #Partida acaba
         return False
 
     #9 jogadas, então acaba e dá velha
-    if (vetor[0] == 8):
+    if (vetor[0] == 9):
         if not simulacao:
             #print("Deu velha")
-            vetor[10] = -2
-            vetor[13]+= 1 #deu velha    
+            vetor[2] = -2
+            vetor[6]+= 1 #deu velha    
         return False 
     return True  
 
-def jogada_aleatoria(vetorAtual):  
+def jogada_aleatoria(vetorAtual):
+    vetor = vetorAtual[1]
     while True:
-        posicao = random.randint(1, 9)
-        if vetorAtual[posicao] == -1:
+        posicao = random.randint(0, 8)
+        if vetor[posicao] == -1:
             return posicao
 
-def jogador_invencivel(vetor, jogador):
+def jogador_invencivel(vetorAtual, jogador):
+    vetor = vetorAtual[1]
     adversario = 2 if jogador == 1 else 1
 
     if vetor[0] == 0 and jogador == 1:
@@ -245,5 +240,9 @@ def jogador_invencivel(vetor, jogador):
 jogo_da_velha(vetorAtual)
 print(vetorAtual)
 print("-----------")
-print(ranks)
+""" for jogos in vetorJogadas:
+    print(jogos) """
+
 print("------------")
+""" for i in teste:
+    print(i) """
